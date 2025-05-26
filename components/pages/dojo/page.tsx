a8430e64514f15af269aa676139d040608b60c53
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-05-26 22:25:13 +0500

    chore: remove console log

--- components/pages/dojo/page.tsx
+++ components/pages/dojo/page.tsx
@@ -272,8 +272,6 @@ const DojoPage = () => {
   useEffect(() => {
     if (wallet?.address) {
       getOwnedAlienParts(wallet?.address).then((res) => {
-        console.log("res.data ==>", res.data)
-
         if (res.data && Array.isArray(res.data?.userAlienParts)) {
           // Initialize parts map
           const partsMap: Record<string, any[]> = {
