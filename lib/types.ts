2915948e38aa61eb0785acc99a79d300f92ad83f
Nikita Schastnyy <smynay@yandex.ru>
2026-02-26 11:46:49 +0300

    feat: actualize mocks and types

--- lib/types.ts
+++ lib/types.ts
@@ -2,24 +2,24 @@ export interface GetPortfolioItem {
   name: string
   price: string
   amount: string
-  price_diff_24h: string
-  price_diff_positive: boolean
+  price_diff_percentage?: string
+  price_diff_positive?: boolean
   picture_url: string
 }
 
 export interface GetPortfolioCharacter {
   name: string
   price: string
-  amount: string
   picture_url: string
 }
 
 export interface GetPortfolioResponse {
-  zone_balance: string
-  zone_balance_usd: string
-  zone_balance_usd_diff: string
-  zone_balance_diff_percentage: string
-  balance_diff_positive: boolean
+  balance_summary: string
+  balance_usd: string
+  usd_balance_diff?: string
+  usd_diff_percentage?: string
+  usd_balance_diff_positive?: boolean
+  wallet_zone_balance: string
   characters: GetPortfolioCharacter[]
   items: GetPortfolioItem[]
 }
